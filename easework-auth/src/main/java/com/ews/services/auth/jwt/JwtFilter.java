700e3eb4bee75163010c708622a9b0d835349687
LaxmiNarayana Jena <145702@TCSGEGDC.com>
2016-03-04 20:04:34 +0530

    JWT Auth Details updated

--- easework-auth/src/main/java/com/ews/services/auth/jwt/JwtFilter.java
+++ easework-auth/src/main/java/com/ews/services/auth/jwt/JwtFilter.java
@@ -2,6 +2,7 @@ package com.ews.services.auth.jwt;
 
 import java.io.IOException;
 
+import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -11,7 +12,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.AbstractAuthenticationProcessingFilter;
-import org.springframework.stereotype.Component;
 import org.springframework.util.Assert;
 
 public class JwtFilter extends AbstractAuthenticationProcessingFilter {
@@ -39,4 +39,14 @@ public class JwtFilter extends AbstractAuthenticationProcessingFilter {
             javax.servlet.http.HttpServletResponse response) {
 		return true;
 	}
+	
+	@Override
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult)
+            throws IOException, ServletException {
+        super.successfulAuthentication(request, response, chain, authResult);
+
+        // As this authentication is in HTTP header, after success we need to continue the request normally
+        // and return the response as if the resource was not secured at all
+        chain.doFilter(request, response);
+    }
 }
